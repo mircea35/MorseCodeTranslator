ad818d67c0ea5f03b5290c360c3f8917f6a652af
Mircea Ionescu <42213952+mircea35@users.noreply.github.com>
2024-04-23 01:39:44 +0100

    Initial Commit

---
+++ MorseCode/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+
+internal class Program { 
+    static Dictionary<String, String> translationSet = new Dictionary<String, String>();
+    static List<string> morseCode = new List<string>();
+
+    static void importTranslationSet(int standard){
+        string path = "";
+        if(standard == 1){
+            path = @"TranslationSets/international.txt";
+        }else{
+            path = @"TranslationSets/american.txt";
+        }
+        try{
+            foreach(String line in File.ReadLines(path)){
+                if(String.IsNullOrEmpty(line) || line.StartsWith("#")){
+                    continue;
+                }
+                else{
+                    string[] characters = line.Split(" ");
+                    translationSet.Add(characters[0], characters[1]);
+                }
+            }
+        }
+        catch{
+            Console.WriteLine("Importing the Translation set failed. Check if you have the files in the TranslationSets folder/directory.");
+        }
+    }
+
+    static void translateToMorse(string text){
+        char[] characters = new char[text.Length];
+
+        for(int i = 0; i < text.Length; i++){
+            characters[i] = text[i];
+        }
+
+        foreach(char c in characters){
+           if(translationSet.ContainsKey(c.ToString())){
+                morseCode.Add(translationSet[c.ToString()]);
+                morseCode.Add(" ");
+           }else{
+                morseCode.Add("|");
+           }
+        }
+
+        foreach (string item in morseCode)
+        {
+            Console.Write(item);
+        }
+    }
+
+    static public void Main(String[] args) 
+    { 
+        bool keepAlive = true;
+        while(keepAlive){
+            Console.WriteLine("=== Morse Code Translator ===");
+            Console.WriteLine("Main Menu");
+            Console.WriteLine("Choose from the following: ");
+            Console.WriteLine("1. Encoding to morse code.");
+            Console.WriteLine("2. Decoding from morse code.");
+            Console.WriteLine("Input: ");
+            int userChoice = Int32.Parse(Console.ReadLine());
+            switch(userChoice){
+                case 1:
+                    Console.WriteLine("Choose the standard: ");
+                    Console.WriteLine("1. International");
+                    Console.WriteLine("2. American.");
+                    Console.WriteLine("Input: ");
+                    userChoice = Int32.Parse(Console.ReadLine());
+                    importTranslationSet(userChoice);
+                    Console.WriteLine("Input the line: ");
+                    string userInput = Console.ReadLine();
+                    translateToMorse(userInput);
+                    break;
+            }
+        }
+
+    }
+}
